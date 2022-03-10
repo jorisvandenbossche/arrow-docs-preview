dd403741e596a1bd3c813e0ba039a2537bef6f56
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2022-03-10 13:05:24 +0100

    Update splitted docs

--- html-splitted/python/_static/documentation_options.js
+++ html-splitted/python/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '7.0.0 (dev)',
+    VERSION: '8.0.0 (dev)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
